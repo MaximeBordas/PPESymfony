00a18f1b5774b4317bd7e1dfda3b1df9ab6b1443
Maxime Bordas <maxime.bordas01@gmail.com>
2017-03-14 12:08:39 +0100

    Ajout de la structure du Bundle de Facturation

---
+++ src/FacturationBundle/Controller/FacturationController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxime
+ * Date: 14/03/2017
+ * Time: 11:50
+ */
+
+namespace FacturationBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class FacturationController extends Controller
+{
+    public  function  indexAction()
+    {
+        return $this->render('FacturationBundle:Facture:index.html.twig');
+    }
+
+}
