cc1bd6e3013129102ea0b07e7b11062f2441df56
SebthedevFR <s.toursel@gmail.com>
2017-03-17 15:27:20 +0100

    ajoute bouton facturation

---
+++ src/FacturationBundle/EventListener/MyMenuItemListListener.php
@@ -0,0 +1,48 @@
+<?php
+namespace FacturationBundle\EventListener;
+
+// ...
+
+use Avanzu\AdminThemeBundle\Model\MenuItemModel;
+use Avanzu\AdminThemeBundle\Event\SidebarMenuEvent;
+use Symfony\Component\HttpFoundation\Request;
+
+class MyMenuItemListListener {
+
+    // ...
+
+    public function onSetupMenu(SidebarMenuEvent $event) {
+
+        $request = $event->getRequest();
+
+        foreach ($this->getMenu($request) as $item) {
+            $event->addItem($item);
+        }
+
+    }
+
+    protected function getMenu(Request $request) {
+        // Build your menu here by constructing a MenuItemModel array
+        $menuItems = array(
+            $blog = new MenuItemModel('ItemId', 'Facturation', 'facturation_accueil', array(/* options */), 'iconclasses fa fa-plane')
+        );
+        return $this->activateByRoute($request->get('_route'), $menuItems);
+    }
+
+    protected function activateByRoute($route, $items) {
+
+        foreach($items as $item) {
+            if($item->hasChildren()) {
+                $this->activateByRoute($route, $item->getChildren());
+            }
+            else {
+                if($item->getRoute() == $route) {
+                    $item->setIsActive(true);
+                }
+            }
+        }
+
+        return $items;
+    }
+
+}
